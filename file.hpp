de76404aff457eb8f0cb963edc1ba0e0f4a8e35c
Johan Medrano <jmsnowrider@gmail.com>
2017-03-07 23:01:50 +0100

    Fixed queues - end of tp1

--- file.hpp
+++ file.hpp
@@ -3,38 +3,51 @@
 
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
-template <typename T> class File; 
+template<typename T>
+class File;
 
-template <typename T> class Cellule { 
-  friend class File<T>; 
-public: 
-  Cellule(T * elem);
-  Cellule(T * elem, Cellule<T> * next, Cellule<T> * prev); 
+template<typename T>
+class Cellule {
+        friend class File<T>;
 
-private: 
-  static int count; 
-  T * element; 
-  Cellule<T> * next = NULL; 
-  Cellule<T> * prev = NULL; 
+    public:
+        Cellule(T *elem);
+
+        Cellule(T *elem, Cellule<T> *next, Cellule<T> *prev);
+
+    private:
+        static int count;
+        T *element;
+        Cellule<T> *next = NULL;
+        Cellule<T> *prev = NULL;
 };
- 
-template <typename T> class File {
-public: 
-  File(); 
-  File(T * elem); 
-  bool empty(); 
-  int len(); 
-  void push_back(T * elem); 
-  void push_front(T * elem);
-  T * pop_back();
-  T * pop_front();   
 
-private: 
-  int size; 
-  Cellule<T> * first;
-  Cellule<T> * last; 
+template <typename T> class File {
+    public:
+        File();
+
+        File(T *elem);
+
+        ~File();
+
+        bool empty();
+
+        int len();
+
+        void push_back(T *elem);
+
+        void push_front(T *elem);
+
+        T *pop_back();
+
+        T *pop_front();
+
+    private:
+        int size;
+        Cellule<T> *first;
+        Cellule<T> *last;
 };
 
 
--- main.cpp
+++ main.cpp
@@ -10,14 +10,46 @@ void test_figure(){
     cout << "Nombre de figures crées : " << tab_figures[3]->getCount() << endl;
 }
 
-void test_files() { 
-  Cellule<int> cel(new int(5), NULL, NULL); 
-  File<int> file_vide; 
-  File<int> file(new int(5)); 
-} 
+void test_files() {
+    File<int> file_vide;
+    File<float> ffile;
+    cout << file_vide.empty() << endl;
+    cout << ffile.empty() << endl;
+    ffile.push_front(new float(50.95));
+    File<int> file(new int(5));
+    file.push_back(new int(6));
+    file.push_back(new int(7));
+    file.push_front(new int(98));
+    cout << (file.empty()) << ", " << file.len() << endl;
+    cout << *(file.pop_front()) << endl;
+    cout << *(file.pop_front()) << endl;
+    cout << file.len() << endl;
+    cout << *(file.pop_back()) << endl;
+    cout << *(file.pop_back()) << endl;
+    cout << file.empty() << endl;
+}
+
+void test_files_figures() {
+    Figure *pelem;
+    File<Figure> file;
+    file.push_back(new Rectangle(2, 5));
+    file.push_back(new Carre(5));
+    file.push_back(new TriangleEquilateral(98));
+    file.push_back(new Cercle(6, "Rouge"));
+    file.push_back(new Cercle(19, "Violet joli"));
+
+    int len = file.len();
+    for (int i = 0; i < len; i++) {
+        cout << i << endl;
+        pelem = file.pop_front();
+        pelem->afficherCaracteristiques();
+        pelem->perimetre();
+    }
+}
 
 int main() {
     test_figure();
-    test_files(); 
+    test_files();
+    test_files_figures();
     return 0;
 }
